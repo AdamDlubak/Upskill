871fe9b2ec3c129197e04878b8ae6da243b29ae7
Adam Dłubak <adlubak@pgs-soft.com>
2018-12-05 04:45:53 +0100

    Singleton

--- DesignPatterns/Singleton/Program.cs
+++ DesignPatterns/Singleton/Program.cs
@@ -4,9 +4,30 @@ namespace Singleton
 {
     class Program
     {
+        public class Shop
+        {
+            private static Shop _instance = new Shop(243);
+
+            public int NumberOfProducts { get; set; }
+
+            public static Shop GetInstance()
+            {
+                return _instance;
+            }
+
+            private Shop(int productsNumber)
+            {
+                NumberOfProducts = productsNumber;
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var instance = Shop.GetInstance();
+
+            Console.WriteLine(instance.NumberOfProducts);
+            Console.ReadKey();
         }
     }
 }
